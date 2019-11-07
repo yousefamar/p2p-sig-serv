f2ea7f420ff326a83cc945bdb1f56b01a857cd41
Yousef Amar <yousefamar@gmail.com>
2019-11-07 19:18:41 +0000

    Implement roomcast and add logging

--- server.js
+++ server.js
@@ -39,6 +39,8 @@ io.on('connection', socket => {
 
 		data.uid = socket.id;
 
+		console.log(data.uid, 'joined', data.rid);
+
 		let room = '';
 		for (let scope of data.rid.split('/')) {
 			room += '/' + scope;
@@ -48,12 +50,32 @@ io.on('connection', socket => {
 		}
 	});
 
+	// Multicast message
+	socket.on('roomcast', data => {
+		if (data.rid == null)
+			return;
+
+		data.uid = socket.id;
+
+		console.log(data.uid, 'roomcasted', data);
+
+		let room = '';
+		for (let scope of data.rid.split('/')) {
+			room += '/' + scope;
+			data.rid = room;
+			socket.broadcast.to(room).emit('roomcast', data);
+		}
+	});
+
 	// Unicast hail
 	socket.on('hail', data => {
 		if (data.to == null)
 			return;
 
 		data.from = socket.id;
+
+		console.log(data.from, 'hailed', data.to);
+
 		socket.broadcast.to(data.to).emit('hail', data);
 	});
 
@@ -64,12 +86,17 @@ io.on('connection', socket => {
 
 		socket.leave(data.rid);
 		data.uid = socket.id;
+
+		console.log(data.uid, 'left', data.to);
+
 		socket.broadcast.to(data.rid).emit('leave', data);
 	});
 
 	// Multicast disconnect
 	socket.on('disconnect', () => {
 		Object.keys(socket.rooms).forEach(room => {
+			console.log(data.uid, 'left', data.to);
+
 			socket.broadcast.to(room).emit('leave', {
 				uid: socket.id
 			});
